121ca79c6f8ae11aeaa6c7126e520c58e581e6e2
David Reinberger <S2020306043@fhooe.at>
2024-10-23 10:29:47 +0200

    feat: add class structure

---
+++ src/main/java/at/fhhagenberg/sqelevator/Floor.java
@@ -0,0 +1,10 @@
+package at.fhhagenberg.sqelevator;
+
+public class Floor {
+    private boolean mButtonUpPressed;
+    private boolean mButtonDownPressed;
+
+    public boolean getButtonUpPressed();
+
+    public boolean getButtonDownPressed();
+}
