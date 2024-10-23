7cdb35e8ab58bbc570a99561a1004239dd76d8b0
David Reinberger <S2020306043@fhooe.at>
2024-10-23 10:30:42 +0200

    feat: add implementation of floor class

--- src/main/java/at/fhhagenberg/sqelevator/Floor.java
+++ src/main/java/at/fhhagenberg/sqelevator/Floor.java
@@ -4,7 +4,13 @@ public class Floor {
     private boolean mButtonUpPressed;
     private boolean mButtonDownPressed;
 
-    public boolean getButtonUpPressed();
+    public boolean getButtonUpPressed()
+    {
+        return mButtonUpPressed;
+    }
 
-    public boolean getButtonDownPressed();
+    public boolean getButtonDownPressed()
+    {
+        return mButtonDownPressed;
+    }
 }
