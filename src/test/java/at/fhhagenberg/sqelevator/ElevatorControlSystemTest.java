b2ff1634aa592b5740b3776d87e5f91de49a7a5c
David Reinberger <S2020306043@fhooe.at>
2024-11-06 15:52:22 +0100

    test: add test with multiple update cycles

--- src/test/java/at/fhhagenberg/sqelevator/ElevatorControlSystemTest.java
+++ src/test/java/at/fhhagenberg/sqelevator/ElevatorControlSystemTest.java
@@ -183,4 +183,117 @@ public class ElevatorControlSystemTest {
         verify(plcMock, times(1)).getTarget(0);
     }
 
+    @Test
+    public void testMultipleUpdates() throws Exception {
+        when(plcMock.getCommittedDirection(0)).thenReturn(ELEVATOR_DIRECTION_UP);
+        when(plcMock.getElevatorAccel(0)).thenReturn(30);
+        when(plcMock.getElevatorButton(0, 0)).thenReturn(true);
+        when(plcMock.getElevatorDoorStatus(0)).thenReturn(ELEVATOR_DOORS_OPEN);
+        when(plcMock.getElevatorFloor(0)).thenReturn(0);
+        when(plcMock.getElevatorSpeed(0)).thenReturn(10);
+        when(plcMock.getElevatorWeight(0)).thenReturn(100);
+        when(plcMock.getElevatorCapacity(0)).thenReturn(5);
+        when(plcMock.getElevatorNum()).thenReturn(1);
+        when(plcMock.getFloorButtonDown(0)).thenReturn(false);
+        when(plcMock.getFloorButtonDown(1)).thenReturn(false);
+        when(plcMock.getFloorButtonDown(2)).thenReturn(false);
+        when(plcMock.getFloorButtonDown(3)).thenReturn(true);
+        when(plcMock.getFloorButtonUp(0)).thenReturn(false);
+        when(plcMock.getFloorButtonUp(1)).thenReturn(false);
+        when(plcMock.getFloorButtonUp(2)).thenReturn(false);
+        when(plcMock.getFloorButtonUp(3)).thenReturn(false);
+        when(plcMock.getFloorNum()).thenReturn(4);
+        when(plcMock.getServicesFloors(0, 1)).thenReturn(false);
+        when(plcMock.getServicesFloors(0, 2)).thenReturn(false);
+        when(plcMock.getServicesFloors(0, 3)).thenReturn(true);
+        when(plcMock.getTarget(0)).thenReturn(3);
+
+        ecs.initializeElevatorsViaPLC();
+
+        ecs.updateDataViaPLC();
+
+        Elevator[] elevators = ecs.getElevators();
+        Floor[] floors = ecs.getFloors();
+
+        assertEquals(elevators[0].getDirection(), ELEVATOR_DIRECTION_UP);
+        assertTrue(elevators[0].getElevatorButton(0));
+        assertEquals(elevators[0].getElevatorDoorStatus(), ELEVATOR_DOORS_OPEN);
+        assertEquals(elevators[0].getCurrentFloor(), 0);
+        assertTrue(elevators[0].getFloorService(3));
+        assertEquals(elevators[0].getSpeed(), 10);
+        assertEquals(elevators[0].getWeight(), 100);
+        assertEquals(elevators[0].getCapacity(), 5);
+        assertEquals(elevators[0].getTargetFloor(), 3);
+        assertEquals(elevators[0].getAcceleration(), 30);
+
+        assertFalse(floors[0].getButtonDownPressed());
+        assertFalse(floors[1].getButtonDownPressed());
+        assertFalse(floors[2].getButtonDownPressed());
+        assertTrue(floors[3].getButtonDownPressed());
+        assertFalse(floors[0].getButtonUpPressed());
+        assertFalse(floors[1].getButtonUpPressed());
+        assertFalse(floors[2].getButtonUpPressed());
+        assertFalse(floors[3].getButtonUpPressed());
+
+        when(plcMock.getFloorButtonDown(2)).thenReturn(true);
+        when(plcMock.getFloorButtonDown(3)).thenReturn(false);
+        when(plcMock.getFloorButtonUp(0)).thenReturn(true);
+        when(plcMock.getServicesFloors(0, 2)).thenReturn(true);
+        when(plcMock.getServicesFloors(0, 3)).thenReturn(false);
+
+        ecs.updateDataViaPLC();
+
+        elevators = ecs.getElevators();
+        floors = ecs.getFloors();
+
+        assertEquals(elevators[0].getDirection(), ELEVATOR_DIRECTION_UP);
+        assertTrue(elevators[0].getElevatorButton(0));
+        assertEquals(elevators[0].getElevatorDoorStatus(), ELEVATOR_DOORS_OPEN);
+        assertEquals(elevators[0].getCurrentFloor(), 0);
+        assertFalse(elevators[0].getFloorService(3));
+        assertTrue(elevators[0].getFloorService(0));
+        assertTrue(elevators[0].getFloorService(2));
+        assertEquals(elevators[0].getSpeed(), 10);
+        assertEquals(elevators[0].getWeight(), 100);
+        assertEquals(elevators[0].getCapacity(), 5);
+        assertEquals(elevators[0].getTargetFloor(), 3);
+        assertEquals(elevators[0].getAcceleration(), 30);
+
+        assertFalse(floors[0].getButtonDownPressed());
+        assertFalse(floors[1].getButtonDownPressed());
+        assertTrue(floors[2].getButtonDownPressed());
+        assertFalse(floors[3].getButtonDownPressed());
+        assertTrue(floors[0].getButtonUpPressed());
+        assertFalse(floors[1].getButtonUpPressed());
+        assertFalse(floors[2].getButtonUpPressed());
+        assertFalse(floors[3].getButtonUpPressed());
+
+
+        verify(plcMock, times(2)).getCommittedDirection(0);
+        verify(plcMock, times(2)).getElevatorAccel(0);
+        verify(plcMock, times(2)).getElevatorButton(0, 0);
+        verify(plcMock, times(2)).getElevatorButton(0, 1);
+        verify(plcMock, times(2)).getElevatorButton(0, 2);
+        verify(plcMock, times(2)).getElevatorButton(0, 3);
+        verify(plcMock, times(2)).getElevatorDoorStatus(0);
+        verify(plcMock, times(2)).getElevatorFloor(0);
+        verify(plcMock, times(2)).getElevatorSpeed(0);
+        verify(plcMock, times(2)).getElevatorWeight(0);
+        verify(plcMock, times(1)).getElevatorCapacity(0);
+        verify(plcMock, times(1)).getElevatorNum();
+        verify(plcMock, times(2)).getFloorButtonDown(0);
+        verify(plcMock, times(2)).getFloorButtonDown(1);
+        verify(plcMock, times(2)).getFloorButtonDown(2);
+        verify(plcMock, times(2)).getFloorButtonDown(3);
+        verify(plcMock, times(2)).getFloorButtonUp(0);
+        verify(plcMock, times(2)).getFloorButtonUp(1);
+        verify(plcMock, times(2)).getFloorButtonUp(2);
+        verify(plcMock, times(2)).getFloorButtonUp(3);
+        verify(plcMock, times(1)).getFloorNum();
+        verify(plcMock, times(2)).getServicesFloors(0, 1);
+        verify(plcMock, times(2)).getServicesFloors(0, 2);
+        verify(plcMock, times(2)).getServicesFloors(0, 3);
+        verify(plcMock, times(2)).getTarget(0);
+    }
+
 }
