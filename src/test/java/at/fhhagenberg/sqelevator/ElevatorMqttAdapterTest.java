7ed389afbfd44e8299a6842d23c585c728d7c0f4
David Reinberger <S2020306043@fhooe.at>
2024-11-28 21:18:28 +0100

    feat: add test for initial update

--- src/test/java/at/fhhagenberg/sqelevator/ElevatorMqttAdapterTest.java
+++ src/test/java/at/fhhagenberg/sqelevator/ElevatorMqttAdapterTest.java
@@ -7,6 +7,8 @@ import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.mockito.junit.jupiter.MockitoSettings;
+import org.mockito.quality.Strictness;
 import org.testcontainers.hivemq.HiveMQContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.utility.DockerImageName;
@@ -22,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
 public class ElevatorMqttAdapterTest {
     @Container
@@ -60,7 +63,7 @@ public class ElevatorMqttAdapterTest {
                 .serverPort(hivemqCe.getMqttPort())
                 .buildAsync();
 
-        publisher.connect().get(1, TimeUnit.SECONDS);
+        publisher.connect().get(2, TimeUnit.SECONDS);
         publisher.publishWith()
                 .topic("elevator_control/connection_status").retain(true)
                 .payload(String.valueOf(true).getBytes()).send();
@@ -90,7 +93,7 @@ public class ElevatorMqttAdapterTest {
         when(plc.getFloorButtonUp(4)).thenReturn(false);
         when(plc.getFloorNum()).thenReturn(5);
         when(plc.getFloorHeight()).thenReturn(3);
-        when(plc.getServicesFloors(0, 0)).thenReturn(false);
+        when(plc.getServicesFloors(0, 0)).thenReturn(true);
         when(plc.getServicesFloors(0, 1)).thenReturn(true);
         when(plc.getServicesFloors(0, 2)).thenReturn(false);
         when(plc.getServicesFloors(0, 3)).thenReturn(false);
@@ -119,7 +122,7 @@ public class ElevatorMqttAdapterTest {
         ConcurrentHashMap<String, String> receivedMessages = new ConcurrentHashMap<>();
         CountDownLatch latch = new CountDownLatch(expectedMessages.size());
 
-        var subscriber = Mqtt5Client.builder()
+        Mqtt5AsyncClient subscriber = Mqtt5Client.builder()
                 .identifier(UUID.randomUUID().toString())
                 .serverHost(hivemqCe.getHost())
                 .serverPort(hivemqCe.getMqttPort())
@@ -127,26 +130,92 @@ public class ElevatorMqttAdapterTest {
 
         subscriber.connect();
         subscriber.subscribeWith()
-                .addSubscription()
-                    .topicFilter("info/#")
-                    .applySubscription()
-                .addSubscription()
-                    .topicFilter("elevator/+/capacity")
-                    .applySubscription()
-                .callback(publish -> {
-                    String payload = new String(publish.getPayloadAsBytes());
-                    if (expectedMessages.containsKey(publish.getTopic().toString()) && payload.equals(expectedMessages.get(publish.getTopic().toString()))) {
-                        receivedMessages.put(publish.getTopic().toString(), payload);
-                        latch.countDown();
-                    }
-                })
-                .send();
+            .addSubscription()
+                .topicFilter("info/#")
+                .applySubscription()
+            .addSubscription()
+                .topicFilter("elevator/+/capacity")
+                .applySubscription()
+            .callback(publish -> {
+                String payload = new String(publish.getPayloadAsBytes());
+                if (expectedMessages.containsKey(publish.getTopic().toString()) && payload.equals(expectedMessages.get(publish.getTopic().toString()))) {
+                    receivedMessages.put(publish.getTopic().toString(), payload);
+                    latch.countDown();
+                }
+            })
+            .send();
 
         client.run(250);
+        latch.await();
 
         // Wait for all messages to be received or timeout after 5 seconds
         assertEquals(expectedMessages, receivedMessages, "Received messages do not match expected messages.");
+        subscriber.disconnect();
+    }
 
+    @Test
+    void testInitialValues() throws Exception {
+        Map<String, String> expectedMessages = Map.ofEntries(
+            Map.entry("elevator/0/current_floor", "1"),
+            Map.entry("elevator/0/acceleration", "15"),
+            Map.entry("elevator/0/door_status", "2"),
+            Map.entry("elevator/0/speed", "5"),
+            Map.entry("elevator/0/weight", "10"),
+            Map.entry("elevator/0/floor_requested/1", "true"),
+            Map.entry("elevator/0/floor_requested/0", "false"),
+            Map.entry("elevator/0/floor_requested/2", "false"),
+            Map.entry("elevator/0/floor_requested/3", "false"),
+            Map.entry("elevator/0/floor_requested/4", "false"),
+            Map.entry("floor/0/button_up", "false"),
+            Map.entry("floor/1/button_up", "false"),
+            Map.entry("floor/2/button_up", "false"),
+            Map.entry("floor/3/button_up", "false"),
+            Map.entry("floor/4/button_up", "false"),
+            Map.entry("floor/0/button_down", "false"),
+            Map.entry("floor/1/button_down", "true"),
+            Map.entry("floor/2/button_down", "false"),
+            Map.entry("floor/3/button_down", "false"),
+            Map.entry("floor/4/button_down", "false"),
+            Map.entry("elevator/0/floor_serviced/0", "true"),
+            Map.entry("elevator/0/floor_serviced/1", "true"),
+            Map.entry("elevator/0/floor_serviced/2", "false"),
+            Map.entry("elevator/0/floor_serviced/3", "false"),
+            Map.entry("elevator/0/floor_serviced/4", "false"),
+            Map.entry("elevator/0/target_floor", "4"),
+            Map.entry("elevator/0/direction", "1")
+        );
+
+        ConcurrentHashMap<String, String> receivedMessages = new ConcurrentHashMap<>();
+        CountDownLatch latch = new CountDownLatch(expectedMessages.size());
+
+        Mqtt5AsyncClient subscriber = Mqtt5Client.builder()
+                .identifier(UUID.randomUUID().toString())
+                .serverHost(hivemqCe.getHost())
+                .serverPort(hivemqCe.getMqttPort())
+                .buildAsync();
+
+        subscriber.connect();
+        subscriber.subscribeWith()
+            .addSubscription()
+            .topicFilter("elevator/#")
+            .applySubscription()
+            .addSubscription()
+            .topicFilter("floor/#")
+            .applySubscription()
+            .callback(publish -> {
+                String payload = new String(publish.getPayloadAsBytes());
+                if (expectedMessages.containsKey(publish.getTopic().toString()) && payload.equals(expectedMessages.get(publish.getTopic().toString()))) {
+                    receivedMessages.put(publish.getTopic().toString(), payload);
+                    latch.countDown();
+                }
+            })
+            .send();
+
+        client.run(250);
+
+        latch.await();
+        // Wait for all messages to be received or timeout after 5 seconds
+        assertEquals(expectedMessages, receivedMessages, "Received messages do not match expected messages.");
         subscriber.disconnect();
     }
 }
