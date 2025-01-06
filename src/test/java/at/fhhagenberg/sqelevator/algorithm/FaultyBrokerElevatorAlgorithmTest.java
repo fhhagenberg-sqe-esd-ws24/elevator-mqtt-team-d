5078d2243864858acf237d214244ec90459cf01e
Phillip Oberndorfer <s2310567016@fhooe.at>
2025-01-06 12:01:08 +0100

    refactor: fix code smell regarding sleep in test

--- src/test/java/at/fhhagenberg/sqelevator/algorithm/FaultyBrokerElevatorAlgorithmTest.java
+++ src/test/java/at/fhhagenberg/sqelevator/algorithm/FaultyBrokerElevatorAlgorithmTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
+
 import com.hivemq.client.mqtt.mqtt5.Mqtt5AsyncClient;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
@@ -12,8 +13,10 @@ import org.mockito.junit.jupiter.MockitoExtension;
 import org.mockito.junit.jupiter.MockitoSettings;
 import org.mockito.quality.Strictness;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
+
+import java.util.concurrent.TimeUnit;
+import static org.awaitility.Awaitility.await;
 
 @MockitoSettings(strictness = Strictness.LENIENT)
 @ExtendWith(MockitoExtension.class)
@@ -43,10 +46,8 @@ class FaultyBrokerElevatorAlgorithmTest {
             }
         });
         testThread.start();
-        Thread.sleep(6000);
-
-        assertTrue(baos.toString().contains("Failed to connect to broker. Retrying in 5 seconds..."));
-        verify(mockMqttClient, times(2)).connect();
+        await().atMost(6, TimeUnit.SECONDS).until(() -> baos.toString().contains("Failed to connect to broker. Retrying in 5 seconds..."));
+        verify(mockMqttClient, times(1)).connect();
 
         testThread.interrupt();
         testThread.join();
